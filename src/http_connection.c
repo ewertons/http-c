65bf1a17e5b908fbc8064e7e0f5b3f4d25e2dadc
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2025-01-30 11:57:26 -0800

    Fix memory leak

--- src/http_connection.c
+++ src/http_connection.c
@@ -135,6 +135,12 @@ result_t http_connection_close(http_connection_t* connection)
     }
     else
     {
+        // TODO: add a connection_state flag to know if it is initialized.
+        (void)socket_deinit(&connection->socket);
+
+        memset(&connection->stream, 0, sizeof(stream_t)); // TODO: add a stream_close and call it here.
+        connection->endpoint = NULL;
+
         result = ok;
     }
 
